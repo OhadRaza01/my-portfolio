29d32a46c674e4ce9492386ff2aefa9d154288d8
OhadRaza01 <ohadraza345@gmail.com>
2026-06-19 15:59:03 +0500

    added smooth scrolling using lenis

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactLenis from 'lenis/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ReactLenis root options={{
+      lerp: 0.08,
+      smoothTouch: false
+    }}>
+      <App />
+    </ReactLenis>
   </StrictMode>,
 )
